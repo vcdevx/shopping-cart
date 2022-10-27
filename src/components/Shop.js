64e12e35dfbfd81edff71b5e0e5584ee9010cf6e
Van <vcdevx@gmail.com>
2022-10-26 23:58:29 -0400

    Refactoring shop items into json for more efficient access

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import { Link } from "react-router-dom";
+import Collection from "./collection/Collection.json";
 
 const Shop = () => {
 
@@ -78,17 +79,18 @@ const Shop = () => {
         },
     ])
 
+    const shoeData = Collection;
+
     return (
         <div className="container-fluid row row-cols-1 row-cols-md-3 g-4 mt-5">
-            {inventory.map((item) => (
-                <Link to={`/shop/${item.id}`}>
+            {shoeData.map((item) => (
+                <Link key={item.id} to={`/shop/${item.id}`}>
                 <div className="col">
                 <div className="card" style={{width: 18 + 'rem'}} key={item.id}>
-                <img src={item.image} className="card-img-top" alt={item.name}/>
+                <img src={item.img} className="card-img-top" alt={item.name}/>
                 <div className="card-body">
                   <h5 className="card-title">{item.name}</h5>
                   <p className="card-text">{`$${item.price}`}</p>
-                  <button className="btn btn-primary">Buy</button>
                 </div>
               </div>     
               </div>
