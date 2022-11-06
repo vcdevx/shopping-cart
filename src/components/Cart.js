74a251c55a125cad5a8901340f0b0f96f728d73a
Van <vcdevx@gmail.com>
2022-11-06 18:06:11 -0500

    Adjusted margins for cart component

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,68 +1,72 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 const Cart = (props) => {
+  const { cartItems, setCartItems, cartCount, setCartCount } = props;
 
-    const {cartItems, setCartItems, cartCount, setCartCount} = props;
+  const [total, setTotal] = useState("");
 
-    const [total, setTotal] = useState('')
+  useEffect(() => {
+    updateCartCount();
+    updateTotal();
+  });
 
-    useEffect(() => {
-        updateCartCount()
-        updateTotal()
-    })
-
-    const updateCartCount = () => {
-        let totalItems = 0;
-        for (let i = 0; cartItems.length > i; i++) {
-            totalItems += cartItems[i].quantity
-        }
-
-        setCartCount(totalItems)
+  const updateCartCount = () => {
+    let totalItems = 0;
+    for (let i = 0; cartItems.length > i; i++) {
+      totalItems += cartItems[i].quantity;
     }
 
-    const updateTotal = () => {
-        let total = 0;
-        for (let i = 0; cartItems.length > i; i++) {
-            total = total + (cartItems[i].quantity * cartItems[i].price)
-        }
+    setCartCount(totalItems);
+  };
 
-        setTotal(total)
+  const updateTotal = () => {
+    let total = 0;
+    for (let i = 0; cartItems.length > i; i++) {
+      total = total + cartItems[i].quantity * cartItems[i].price;
     }
 
-    const removeItem = (e) => {
-        let key = e.target.getAttribute('id')
-        let item = cartItems.find(shoe => shoe.shoeId === key)
-        let newCart = [...cartItems.filter(shoe => shoe !== item)]
-        setCartItems(newCart)
-    }
+    setTotal(total);
+  };
 
-    return (
-        <div className="container-fluid">
-            <h2>Shopping Bag</h2>
-            <div className="container">
-                {cartItems.map((item) => (
-                    <div className="border-top border-bottom" key={item.shoeId}>
-                        <div className="row">
-                            <div className="col-sm-2">
-                                <img src={item.img} class="img-fluid p-3"/>
-                                </div>
-                                <div className="col-sm-8 p-3">
-                        <p>{item.name}</p>
-                        <p>{item.size}</p>
-                        <p>Quantity: {item.quantity}</p>
-                        </div>
-                        <div className="col-sm-2">
-                            <p>{item.price}</p>
-                            </div>
-                        <button onClick={removeItem} id={item.shoeId}>Remove</button>
-                        </div>
-                    </div>
-                ))}
-                <p>Total Items: {cartCount}</p>
-                <p>Total: ${total}</p>
+  const removeItem = (e) => {
+    let key = e.target.getAttribute("id");
+    let item = cartItems.find((shoe) => shoe.shoeId === key);
+    let newCart = [...cartItems.filter((shoe) => shoe !== item)];
+    setCartItems(newCart);
+  };
+
+  return (
+    <div className="container-fluid row mt-5">
+      <div className="container col-sm-6">
+      <h4>Bag</h4>
+        {cartItems.map((item) => (
+          <div className="border-top border-bottom" key={item.shoeId}>
+            <div className="row">
+              <div className="col-sm-3">
+                <img src={item.img} class="img-fluid p-3" />
+              </div>
+              <div className="col-sm-6 p-3">
+                <p>{item.name}</p>
+                <p className="text-secondary">Size: {item.size}</p>
+                <p className="text-secondary">QTY: {item.quantity}</p>
+              </div>
+              <div className="col-sm-3 mt-3">
+                <p>${item.price}</p>
+              </div>
+              <button onClick={removeItem} id={item.shoeId}>
+                Remove
+              </button>
             </div>
-        </div>
-    )
-}
+          </div>
+        ))}
+      </div>
+      <div className="container col-sm-3 ">
+        <h4>Summary</h4>
+        <p>Total Items: {cartCount}</p>
+        <p>Total: ${total}</p>
+      </div>
+    </div>
+  );
+};
 
 export default Cart;
